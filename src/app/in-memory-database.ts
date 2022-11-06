b971cc322faf8dc28a7114aa11c5ae496b80222b
nayland <nayland@totvs.com.br>
2022-11-06 20:01:20 -0300

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,16 +1,18 @@
 import {InMemoryDbService, RequestInfo} from 'angular-in-memory-web-api'
+import {Category} from "./pages/categories/shared/category.model.ts"
 
 export class InMemoryDatabase implements InMemoryDbService{
 
-createDb: any  {
-  const categorias =[
+createDb()  {
+  const categories: Category[]=[
     {id: 1, name: 'Moradia' , description: 'Pagamento de contas da casa' },
-    {id: 2, name: 'Saúde' , description: 'Pagamento de contas da casa' },
-    {id: 3, name: 'Lazer' , description: 'Pagamento de contas da casa' },
-    {id: 4, name: 'Salário' , description: 'Pagamento de contas da casa' },
-    {id: 5, name: 'Frelance' , description: 'Pagamento de contas da casa' }
+    {id: 2, name: 'Saúde' , description: 'Plano de saúde e remadios' },
+    {id: 3, name: 'Lazer' , description: 'Cinema, parques, etc..' },
+    {id: 4, name: 'Salário' , description: 'Recebumento de salario' },
+    {id: 5, name: 'Frelance' , description: 'Trabalhos com freelance' }
   ];
 
-  return {categorias}
+  return {categories}
 
+  }
 }
